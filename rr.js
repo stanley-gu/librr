ced5d32c72406f9a55258287cd421582b1b0cf2b
Stanley Gu <stanleygu@gmail.com>
2013-02-04 23:42:24 -0800

    more calls to library

--- rr.js
+++ rr.js
@@ -7,3 +7,6 @@ console.log(rr.librr_c_api.getWorkingDirectory());
 
 rr.librr_c_api.loadSBMLFromFile('/usr/local/models/feedback.xml')
 console.log(rr.librr_c_api.getCurrentSBML())
+console.log('number of floating species: ' + rr.librr_c_api.getNumberOfFloatingSpecies())
+var results = rr.librr_c_api.simulate();
+console.log('results are: \n' + rr.librr_c_api.resultToString(results))
